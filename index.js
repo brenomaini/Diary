6920a5d051dfd71b614ad706e844fe74b2a84d4a
Breno Maini <dev.brenomaini@gmail.com>
2023-01-05 11:04:11 -0300

    New files created and ui/ux modified

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.render("index", { variavel: "teste" });
+  res.render("home", { variavel: "teste" });
+});
+app.get("/create", (req, res) => {
+  res.render("createProfile", { variavel: "teste" });
+});
+app.get("/login", (req, res) => {
+  res.render("login", { variavel: "teste" });
+});
+
+/* ------------------------------ After Login ---------------------*/
+app.get("/write", (req, res) => {
+  res.render("write", { variavel: "teste" });
 });
 
 app.listen(3000, () => {
